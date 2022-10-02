1d225b7e4f36e722f56bc40ade3b9f1e73f40401
PandaMan <shaefferaelita@gmail.com>
2022-10-03 01:38:25 +0200

    base

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <!-- import html -->
+        <link rel="stylesheet" href="style.css">
+
+    </head>
+    <body>
+        <div id="navbar">
+            <div id="logo">
+                <img id="logopanda" src="./assets/img/PandaMor3ackV2.png" />
+            </div>
+            <div id="propos_nav" style="display: none;">
+                <p class="btn">Mentions légales</p>
+            </div>
+            <div class="navbarbtn btn">
+                <p>A PROPOS</p>
+            </div>
+        </div>
+
+        <h1 id="myproject">MES PROJECT</h1>
+        <div id="project">
+
+        </div>
+    </body>
+</html>
