e6af95bc54a8571319a0a6c606f77ede2d2e594a
PandaMan <shaefferaelita@gmail.com>
2022-10-03 03:39:39 +0200

    end

--- index.php
+++ index.php
@@ -12,21 +12,65 @@
 
     </head>
     <body>
+        <div id="background"></div>
         <div id="navbar">
             <div id="logo">
                 <img id="logopanda" src="./assets/img/PandaMor3ackV2.png" />
             </div>
             <div id="propos_nav" style="display: none;">
-                <p class="btn">Mentions légales</p>
+                <p id="mentionlegale" class="btn">Mentions légales</p>
             </div>
-            <div class="navbarbtn btn">
+            <div class="navbarbtn btn" id="navbarbtn">
                 <p>A PROPOS</p>
             </div>
         </div>
 
         <h1 id="myproject">MES PROJECT</h1>
         <div id="project">
-
+            <div class="projectL">
+                <?php 
+                    $arrFiles = scandir('./assets/project');
+                    $project = array();
+                    $maxl = 1;
+                    foreach($arrFiles as $file)
+                    {
+                        if (!is_dir("./$file"))
+                        {
+                            if($maxl > 3){
+                                $maxl = 1;
+                                echo '</div><div class="projectL">';
+                            }
+                            echo '<div class="projectS btn" id="project_'.str_replace(".png", "", $file).'"><h3>'.str_replace(".png", "", $file).'</h3><img src="./assets/project/'.$file.'"></div>';
+                            $project["project_".str_replace(".png", "", $file)] = str_replace(".png", "", $file);
+                            $maxl = $maxl + 1;
+                        }
+                    }
+                ?>
+                <!-- <div class="projectS btn" id="p1"><h3>PopCode</h3><img src="./assets/img/popcode.png"> </div>
+                <div class="projectS btn" id="p2"><h3>Allojyvé</h3><img src="./assets/img/allojyve.png"></div>
+                 -->
+            </div>
         </div>
+        <script type="text/javascript">
+            const toggleDisplay = target => target.style.display = 
+            (target.style.display == 'none') ?
+                '' :
+                'none';
+            document.getElementById("navbarbtn").addEventListener("click", function(e) {
+                toggleDisplay(document.getElementById("propos_nav"));
+            });
+            document.getElementById("mentionlegale").addEventListener("click", function(e) {
+
+            });
+            <?php
+                // foreatch project for echo key and value
+                foreach ($project as $key => $value) {
+                    echo 'document.getElementById("'.$key.'").addEventListener("click", function(e) {
+                        window.open("https://'.$value.'.pandatown.fr","_blank");
+                    });';
+                }
+
+            ?>
+        </script>
     </body>
 </html>
